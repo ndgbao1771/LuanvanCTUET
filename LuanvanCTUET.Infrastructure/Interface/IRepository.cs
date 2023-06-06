73dc9a2cde8ed0d81b582fb481e08ca477615e11
Nguyen Diep Gia Bao <nguyendiepgiabao1771@gmail.com>
2023-06-06 11:18:47 +0700

    Add project files.

---
+++ LuanvanCTUET.Infrastructure/Interface/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuanvanCTUET.Infrastructure.Interface
+{
+    public interface IRepository<T, K> where T : class
+    {
+        T FindById(K id, params Expression<Func<T, object>>[] includeProperties);
+        T FindSingle(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includeProperties);
+        IQueryable<T> FindAll(params Expression<Func<T, object>>[] includeProperties);
+        IQueryable<T> FindAll(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includeProperties);
+
+        void Add(T entity);
+        void Updated(T entity);
+        void Delete(T entity);
+        void Delete(K id);
+        void DeleteMultiple(List<T> entities);
+
+    }
+}
---
+++ LuanvanCTUET.Infrastructure/Interface/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuanvanCTUET.Infrastructure.Interface
+{
+    public interface IUnitOfWork : IDisposable
+    {
+      /// <summary>
+      /// Call save change
+      /// </summary>
+        void Commit();
+    }
+}
